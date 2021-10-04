139d3c88d89ed992f899d1f74b809de2dc462e24
Leonardo Bonati <leonardo.bonati@gmail.com>
2021-10-04 17:03:14 -0400

    Leaflet

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,17 @@
 ---
 
 {% include js/conference.js %}
+
+(function() {
+    let map = window.conference.map;
+
+    if (typeof map !== 'undefined') {
+        var main_station = L.marker([47.37785, 8.54035], {
+            icon: L.divIcon({
+                className: '',
+                html: '<span class="fas fa-train"></span> Main Station',
+                iconSize: [120, 56]
+            })
+        }).addTo(map);
+    }
+})();
